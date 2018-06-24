7615eaafd8297444a6903fbfc2870920fff206b8
Roshakar <fake@roshakar.com>
2018-06-23 22:33:55 -0500

    Added an example of xml bean configuration.

--- src/main/java/com/rosh/akar/jokes/JokesApplication.java
+++ src/main/java/com/rosh/akar/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package com.rosh.akar.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rosh/akar/jokes/config/ChuckConfiguration.java
+++ src/main/java/com/rosh/akar/jokes/config/ChuckConfiguration.java
@@ -1,14 +1,11 @@
 package com.rosh.akar.jokes.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-	@Bean
+	// @Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
